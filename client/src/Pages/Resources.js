708956edd8f58ee8c92c760caea796f669c9f63d
Marcia <marciaadler718@gmail.com>
2021-07-27 01:14:42 -0400

    added additional resources to resources page

--- client/src/Pages/Resources.js
+++ client/src/Pages/Resources.js
@@ -8,6 +8,11 @@ export default function Resources() {
       <h6>For all Job Seekers</h6>
       <ListGroup className="resources-listgroup">
         <ListGroup.Item>
+          <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+            LinkedIn
+          </a>
+        </ListGroup.Item>
+        <ListGroup.Item>
           <a href="https://Wonsulting.com" target="_blank" rel="noreferrer">
             Wonsulting.com
           </a>
@@ -33,6 +38,11 @@ export default function Resources() {
             rel="noreferrer"
           >
             Glassdoor
+          </a>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <a href="https://jobs.all-hands.us/" target="_blank" rel="noreferrer">
+            All Hands Job Board
           </a>
         </ListGroup.Item>
       </ListGroup>
@@ -61,6 +71,20 @@ export default function Resources() {
             PowerToFly
           </a>
         </ListGroup.Item>
+        <ListGroup.Item>
+          <a
+            href="https://www.womenwhocode.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Women Who Code
+          </a>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <a href="https://remotewoman.com/" target="_blank" rel="noreferrer">
+            RemoteWoman.com
+          </a>
+        </ListGroup.Item>
       </ListGroup>
       <h6 className="mt-2">Tech Job Seekers</h6>
       <ListGroup className="resources-listgroup">
@@ -72,6 +96,76 @@ export default function Resources() {
         <ListGroup.Item>
           <a href="https://angel.co/jobs" target="_blank" rel="noreferrer">
             AngelList
+          </a>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <a
+            href="https://www.diversifytech.co/job-board"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Diversify Tech Job Board
+          </a>
+        </ListGroup.Item>
+      </ListGroup>
+      <h6 className="mt-2">Remote Job Seekers</h6>
+      <ListGroup className="resources-listgroup">
+        <ListGroup.Item>
+          <a
+            href="https://www.remotelyfirst.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Remotely First Job Board
+          </a>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <a href="https://remotists.com/" target="_blank" rel="noreferrer">
+            Remotists Job Board
+          </a>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <a
+            href="https://weworkremotely.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            We Work Remotely
+          </a>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <a href="https://remotepoc.com/" target="_blank" rel="noreferrer">
+            RemotePOC.com: Best Jobs for remote professionals of Color
+          </a>
+        </ListGroup.Item>
+      </ListGroup>
+      <h6 className="mt-2">Startups</h6>
+      <ListGroup className="resources-listgroup">
+        <ListGroup.Item>
+          <a
+            href="https://ycombinator.getro.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Y Combinator Job Board
+          </a>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <a
+            href="https://jobs.revolution.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Revolution Job Board
+          </a>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <a
+            href="https://www.venwise.com/talent-2/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Venwise
           </a>
         </ListGroup.Item>
       </ListGroup>
