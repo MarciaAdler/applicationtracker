82a25eccc542623b0e35d0b2c53fb5f2cc056494
Marcia <marciaadler718@gmail.com>
2021-08-06 15:19:39 -0400

    created company analytics component - to be updated later

--- client/src/Pages/Analytics.js
+++ client/src/Pages/Analytics.js
@@ -1,10 +1,17 @@
 import React from "react";
 import UserAnalytics from "../components/UserAnalytics";
+import { useStoreContext } from "../utils/GlobalState";
+import CompanyAnalytics from "../components/CompanyAnalytics";
 export default function Analytics() {
+  const [state, dispatch] = useStoreContext();
   return (
     <div className="home-container">
       <h2 className="mt-2 text-center">Analytics</h2>
-      <UserAnalytics />
+      {state.currentUser.role === "Job Seeker" ? (
+        <UserAnalytics />
+      ) : (
+        <CompanyAnalytics />
+      )}
     </div>
   );
 }
---
+++ client/src/components/CompanyAnalytics.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CompanyAnalytics() {
+  return <div>Analytics here</div>;
+}
