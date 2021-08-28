d8c3778620528cf4aadcdf17adde9698bb446099
Marcia <marciaadler718@gmail.com>
2021-08-28 10:37:23 -0400

    hid inactive search analytics

--- client/src/components/UserAnalytics.js
+++ client/src/components/UserAnalytics.js
@@ -100,7 +100,7 @@ export default function Analytics() {
             </Card.Body>
           </Card>
         </Col>
-        <Col className="col-12 col-md-6 col-lg-6 search-column">
+        {/* <Col className="col-12 col-md-6 col-lg-6 search-column">
           <Card className="analytics-card">
             <Card.Body>
               <Card.Title>Number of Searches</Card.Title>
@@ -110,7 +110,7 @@ export default function Analytics() {
               <Card.Text>{interview1Count}</Card.Text>
             </Card.Body>
           </Card>
-        </Col>
+        </Col> */}
       </Row>
       <Row>
         <Col className="col-12 col-md-6 col-lg-6 search-column">
